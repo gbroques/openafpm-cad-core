6254b924620b34d39aeca814806028bf0bcf5faa
G Roques <groques360@gmail.com>
2021-01-23 19:54:04 -0600

    Add threads to hub

--- openafpm_wind_turbine/__init__.py
+++ openafpm_wind_turbine/__init__.py
@@ -1,13 +1,16 @@
 import os
 from abc import ABC, abstractmethod
 
+import Draft
 import FreeCAD as App
-from FreeCAD import Vector, Placement
 import importWebGL
+import Part
+from FreeCAD import Placement, Vector, Rotation
 
 from .alternator import make_alternator
 from .hub import make_hub
 from .master_of_puppets import create_master_of_puppets
+from .common import make_compound
 
 # T Shape
 # =======
@@ -17,6 +20,7 @@ hub_holes_placement = 50
 magnet_length = 46
 hub_holes = 6
 holes = 6
+hub_rod_length = 330
 
 # H Shape
 # =======
@@ -26,6 +30,7 @@ holes = 6
 # magnet_length = 46
 # hub_holes = 7
 # holes = 6
+# hub_rod_length = 250
 
 # Star Shape
 # ==========
@@ -35,11 +40,7 @@ holes = 6
 # magnet_length = 58
 # hub_holes = 8
 # holes = 7
-
-# For threads, move in positive X plane direction by hub holes placement
-# T Shape - 330
-# H Shape - 250
-# Star Shape - 270
+# hub_rod_length = 270
 
 magn_afpm_parameters = {
     'RotorDiskRadius': rotor_radius,
@@ -55,16 +56,17 @@ magn_afpm_parameters = {
 }
 
 user_parameters = {
-    'HubHolesPlacement': hub_holes_placement, # Distance between center of hub hole and center of alternator
+    # Distance between center of hub hole and center of alternator
+    'HubHolesPlacement': hub_holes_placement,
     'RotorInnerCircle': rotor_inner_circle,
-    'Holes': holes, # Radius of outer holes on stator
+    'Holes': holes,  # Radius of outer holes on stator
     'MetalLengthL': 80,
     'MetalThicknessL': 8,
     'FlatMetalThickness': 10,
     'YawPipeRadius': 58.15,
     'PipeThickness': 6,
     'ResineRotorMargin': 5,
-    'HubHoles': hub_holes # Radius of hub holes
+    'HubHoles': hub_holes  # Radius of hub holes
 }
 
 furling_tool_parameters = {
@@ -95,23 +97,27 @@ def main():
         furling_tool_parameters)
     master_of_puppets_doc.recompute()
 
-    wind_turbine = create_wind_turbine(magn_afpm_parameters)
+    wind_turbine = create_wind_turbine(magn_afpm_parameters, user_parameters)
     wind_turbine.render()
 
 
 class WindTurbine(ABC):
     def __init__(self,
                  magn_afpm_parameters,
+                 user_parameters,
                  base_dir,
                  has_separate_master_files,
                  distance_between_stator_and_rotor,
                  flange_bottom_pad_length,
-                 flange_top_pad_length):
+                 flange_top_pad_length,
+                 number_of_hub_holes):
         self.magn_afpm_parameters = magn_afpm_parameters
+        self.user_parameters = user_parameters
         self.has_separate_master_files = has_separate_master_files
         self.distance_between_stator_and_rotor = distance_between_stator_and_rotor
         self.flange_bottom_pad_length = flange_bottom_pad_length
         self.flange_top_pad_length = flange_top_pad_length
+        self.number_of_hub_holes = number_of_hub_holes
 
         self.base_path = os.path.join(
             os.path.dirname(__file__), 'documents', base_dir)
@@ -139,10 +145,21 @@ class WindTurbine(ABC):
             hub_name,
             self.flange_top_pad_length)
         self._move_hub(hub)
+        hub_z_offset = self.calculate_hub_z_offset()
+        middle_flange_pad_thickness = 15
+        thread_z_offset = hub_z_offset + middle_flange_pad_thickness
+        threads = create_threads(self.doc,
+                                 self.user_parameters['HubHoles'],
+                                 hub_rod_length,
+                                 self.number_of_hub_holes,
+                                 self.user_parameters['HubHolesPlacement'],
+                                 thread_z_offset)
+        thread_compound = make_compound(self.doc, 'Threads', threads)
         self.doc.recompute()
         objects = [
             alternator,
-            hub
+            hub,
+            thread_compound
         ]
         importWebGL.export(objects, 'wind-turbine-webgl.html')
 
@@ -164,13 +181,15 @@ def _open_master(base_path):
 
 
 class TShapeWindTurbine(WindTurbine):
-    def __init__(self, magn_afpm_parameters):
+    def __init__(self, magn_afpm_parameters, user_parameters):
         super().__init__(magn_afpm_parameters,
+                         user_parameters,
                          base_dir='t_shape',
                          has_separate_master_files=True,
                          distance_between_stator_and_rotor=30,
                          flange_bottom_pad_length=30,
-                         flange_top_pad_length=30)
+                         flange_top_pad_length=30,
+                         number_of_hub_holes=4)
 
     def calculate_hub_z_offset(self):
         return calculate_hub_z_offset(
@@ -183,13 +202,15 @@ class TShapeWindTurbine(WindTurbine):
 
 
 class HShapeWindTurbine(WindTurbine):
-    def __init__(self, magn_afpm_parameters):
+    def __init__(self, magn_afpm_parameters, user_parameters):
         super().__init__(magn_afpm_parameters,
+                         user_parameters,
                          base_dir='h_shape',
                          has_separate_master_files=True,
                          distance_between_stator_and_rotor=36,
                          flange_bottom_pad_length=15,
-                         flange_top_pad_length=30)
+                         flange_top_pad_length=30,
+                         number_of_hub_holes=5)
 
     def calculate_hub_z_offset(self):
         return calculate_hub_z_offset(
@@ -202,13 +223,15 @@ class HShapeWindTurbine(WindTurbine):
 
 
 class StarShapeWindTurbine(WindTurbine):
-    def __init__(self, magn_afpm_parameters):
+    def __init__(self, magn_afpm_parameters, user_parameters):
         super().__init__(magn_afpm_parameters,
+                         user_parameters,
                          base_dir='star_shape',
                          has_separate_master_files=False,
                          distance_between_stator_and_rotor=45,
                          flange_bottom_pad_length=45,
-                         flange_top_pad_length=40)
+                         flange_top_pad_length=40,
+                         number_of_hub_holes=6)
 
     def calculate_hub_z_offset(self):
         stator_thickness = self.magn_afpm_parameters['CoilInnerWidth1']
@@ -219,14 +242,14 @@ class StarShapeWindTurbine(WindTurbine):
         )
 
 
-def create_wind_turbine(magn_afpm_parameters):
+def create_wind_turbine(magn_afpm_parameters, user_parameters):
     rotor_radius = magn_afpm_parameters['RotorDiskRadius']
     if 0 <= rotor_radius < 187.5:
-        return TShapeWindTurbine(magn_afpm_parameters)
+        return TShapeWindTurbine(magn_afpm_parameters, user_parameters)
     elif 187.5 <= rotor_radius <= 275:
-        return HShapeWindTurbine(magn_afpm_parameters)
+        return HShapeWindTurbine(magn_afpm_parameters, user_parameters)
     else:
-        return StarShapeWindTurbine(magn_afpm_parameters)
+        return StarShapeWindTurbine(magn_afpm_parameters, user_parameters)
 
 
 def calculate_hub_z_offset(coil_inner_width1,
@@ -245,3 +268,38 @@ def calculate_hub_z_offset(coil_inner_width1,
         rotor_resin_cast_thickness +
         distance_between_stator_and_rotor
     )
+
+
+def create_threads(document, radius, length, n, hub_holes_placement, z_offset):
+    thread = document.addObject('Part::Cylinder', 'Thread')
+    thread.Placement = Placement(
+        Vector(0, 0, z_offset), Rotation(Vector(0, 1, 0), 180))
+    thread.Radius = radius
+    thread.Height = length
+    return create_polar_array(thread, n, hub_holes_placement)
+
+
+def create_polar_array(part, n, y_offset):
+    array = [part]
+    Draft.move(part, Vector(0, y_offset, 0))
+    exterior_angle = calculate_exterior_angle(n)
+    App.DraftWorkingPlane.alignToPointAndAxis(
+        Vector(0, 0, 0), Vector(0, 0, 1), 0)
+    previous = part
+    for i in range(n - 1):
+        clone = Draft.clone(previous)
+        Draft.rotate(clone, exterior_angle, Vector(
+            0, 0, 0), axis=Vector(0, 0, 1), copy=False)
+        array.append(clone)
+        previous = clone
+    return array
+
+
+def calculate_exterior_angle(n):
+    """
+    Calculate exterior angle for an "n" sided regular polygon.
+
+    Reference:
+    https://www.mathsisfun.com/geometry/regular-polygons.html
+    """
+    return 360 / n
