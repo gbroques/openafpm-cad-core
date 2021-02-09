d24011edaa4fb5ae24dbf1578a4ecc3b5b962d42
G Roques <groques360@gmail.com>
2021-02-08 19:14:12 -0600

    Move public functions near top of module; make private start with underscore

--- openafpm_wind_turbine/star_shape_frame.py
+++ openafpm_wind_turbine/star_shape_frame.py
@@ -17,10 +17,10 @@ def assemble_star_shape_frame(document, frame_path, metal_length_l, channel_sect
                                                   frame_path,
                                                   metal_length_l,
                                                   channel_section_height)
-    left_middle_bracket, right_middle_bracket = make_middle_brackets(document,
-                                                                     frame_path,
-                                                                     metal_length_l,
-                                                                     channel_section_height)
+    left_middle_bracket, right_middle_bracket = _make_middle_brackets(document,
+                                                                      frame_path,
+                                                                      metal_length_l,
+                                                                      channel_section_height)
     return make_compound(document, 'Frame', [
         channel_section,
         end_bracket,
@@ -30,7 +30,22 @@ def assemble_star_shape_frame(document, frame_path, metal_length_l, channel_sect
     ])
 
 
-def make_middle_brackets(document, frame_path, metal_length_l, channel_section_height):
+def calculate_star_channel_section_height(rotor_radius,
+                                          coil_leg_width,
+                                          metal_length_l):
+    resine_stator_outer_radius = (
+        rotor_radius + coil_leg_width + 20) / cos(radians(30))
+    stator_holes_circle = (
+        rotor_radius + coil_leg_width + 0.5 *
+        (resine_stator_outer_radius - (rotor_radius + coil_leg_width))
+    )
+    return (
+        2 * stator_holes_circle *
+        sqrt(1 - sin(radians(30)) * sin(radians(30))) - metal_length_l
+    )
+
+
+def _make_middle_brackets(document, frame_path, metal_length_l, channel_section_height):
     # Left Middle Bracket
     left_middle_bracket_label = 'LeftMiddleBracket'
     _merge_piece(document, frame_path, left_middle_bracket_label)
@@ -62,18 +77,3 @@ def _merge_piece(document, path, label):
     document.mergeProject(
         os.path.join(path, label + '.FCStd'))
     enforce_recompute_last_spreadsheet(document)
-
-
-def calculate_star_channel_section_height(rotor_radius,
-                                          coil_leg_width,
-                                          metal_length_l):
-    resine_stator_outer_radius = (
-        rotor_radius + coil_leg_width + 20) / cos(radians(30))
-    stator_holes_circle = (
-        rotor_radius + coil_leg_width + 0.5 *
-        (resine_stator_outer_radius - (rotor_radius + coil_leg_width))
-    )
-    return (
-        2 * stator_holes_circle *
-        sqrt(1 - sin(radians(30)) * sin(radians(30))) - metal_length_l
-    )
