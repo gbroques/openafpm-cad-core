74c3f4e72948592bb8ff6380462401eac5395707
G Roques <groques360@gmail.com>
2021-02-20 11:14:08 -0600

    Add yaw bearing research comments

--- openafpm_wind_turbine/__init__.py
+++ openafpm_wind_turbine/__init__.py
@@ -43,6 +43,31 @@ offset = 125
 # yaw_pipe_radius = 57.15
 # offset = 125
 
+# TODO: Yaw bearing
+# T SHAPE
+# =======
+# 1. Top plate body (on Yaw pipe body)
+# 2. Yaw pipe body
+# 3. Low plate body (Outer bottom edge of Yaw pipe body)
+# 4. Hinge inner body (Pipe)
+# 5. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body)
+# 6. Junction to tail body (in betwwen Yaw pipe body and Hinge inner body)
+
+# H SHAPE
+# =======
+# 1. Top plate body (on Yaw pipe body) (TopForAssembly)
+# 2. Side (next to top plate body 90 deg)
+# 3. Yaw pipe body
+# 4. Hinge inner body (Pipe)
+# 5. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body)
+
+# STAR SHAPE
+# ==========
+# 1. Top plate body (on Yaw pipe body) (TopForAssembly)
+# 2. Side (next to top plate body 90 deg)
+# 3. Yaw pipe body
+
+
 magn_afpm_parameters = {
     'RotorDiskRadius': rotor_radius,
     'DiskThickness': 10,
