58dcb893cdc5365345bcbfc50e514a662c57cc55
G Roques <groques360@gmail.com>
2021-03-22 19:17:06 -0500

    Yaw pipe; work in progress

--- openafpm_cad_core/__init__.py
+++ openafpm_cad_core/__init__.py
@@ -6,17 +6,26 @@ from .wind_turbine import create_wind_turbine, WindTurbine
 # 1. Top plate body (on Yaw pipe body)
 # 2. Yaw pipe body
 # 3. Low plate body (Outer bottom edge of Yaw pipe body)
-# 4. Hinge inner body (Pipe)
-# 5. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body)
-# 6. Junction to tail body (in betwwen Yaw pipe body and Hinge inner body)
+# TODO: C-shaped steel arc as a support for wires that pass down the hole at the center of the yaw pipe.
+#       Wires will be secured to this support with cable ties that prevent them moving and chafing.
+
+# Tail Hinge
+# ----------
+# 1. Hinge inner body (Pipe)
+# 2. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body) TODO: Is this needed?
+# 3. Junction to tail body (in betwwen Yaw pipe body and Hinge inner body)
+# TODO: Two side cover pieces over junction
 
 # H SHAPE
 # =======
 # 1. Top plate body (on Yaw pipe body) (TopForAssembly)
 # 2. Side (next to top plate body 90 deg)
 # 3. Yaw pipe body
-# 4. Hinge inner body (Pipe)
-# 5. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body)
+
+# Tail Hinge
+# ----------
+# 1. Hinge inner body (Pipe)
+# 2. Junction under yaw pipe hinge inner (under both Yaw pipe body and Hinge Inner Body) TODO: Is this needed?
 
 # STAR SHAPE
 # ==========
