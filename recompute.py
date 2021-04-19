83e91c1e061abffd48c16a74878a3c3e5455565d
G Roques <groques360@gmail.com>
2021-04-18 19:15:22 -0500

    Update recompute to recompute objects in documents

--- recompute.py
+++ recompute.py
@@ -5,4 +5,6 @@ sort_in_dependency_order = True
 document_by_name = App.listDocuments(sort_in_dependency_order)
 documents = document_by_name.values()
 for document in documents:
+    for obj in doc.Objects:
+        obj.recompute()
     document.recompute(None, True, True)
