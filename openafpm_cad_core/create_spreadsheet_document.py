ac5f043d15a27c5eca6ba503fa6ae7a810472e55
G Roques <groques360@gmail.com>
2021-05-29 11:24:05 -0500

    Add YawBearingTopPlateHoleRadius

--- openafpm_cad_core/create_spreadsheet_document.py
+++ openafpm_cad_core/create_spreadsheet_document.py
@@ -60,7 +60,8 @@ def _get_calculated_parameters():
     return {
         'ResineStatorOuterRadius': '=RotorDiskRadius < 275 ? (RotorDiskRadius + CoilLegWidth + 20) : (RotorDiskRadius + CoilLegWidth + 20) / cos(30)',
         'YawPipeScaleFactor': '=RotorDiskRadius < 187.5 ? 0.95 : 0.9',
-        'YawPipeLength': '=RotorDiskRadius * YawPipeScaleFactor * 2'
+        'YawPipeLength': '=RotorDiskRadius * YawPipeScaleFactor * 2',
+        'YawBearingTopPlateHoleRadius': '=RotorDiskRadius < 187.5 ? 10 : 15'
     }
 
 
