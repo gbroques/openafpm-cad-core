1cb2643a896c8d93c0f3a6242e4aea4ecf0e1208
G Roques <groques360@gmail.com>
2021-07-09 11:37:18 -0500

    Load parameters from JSON file

--- openafpm_cad_core/create_spreadsheet_task_panel.py
+++ openafpm_cad_core/create_spreadsheet_task_panel.py
@@ -3,7 +3,9 @@ FreeCAD macro to create wind turbine document
 with spreadsheet containing input parameters.
 """
 
+import json
 from enum import Enum, unique
+from pathlib import Path
 from typing import Any, Callable
 
 import FreeCAD as App
@@ -20,131 +22,12 @@ class WindTurbine(Enum):
     STAR_SHAPE = 'Star Shape'
 
 
-parameters_by_variant = {
-    WindTurbine.T_SHAPE: {
-        'MagnAFPM': {
-            'RotorDiskRadius': 150,
-            'DiskThickness': 10,
-            'MagnetLength': 46,
-            'MagnetWidth': 30,
-            'MagnetThickness': 10,
-            'NumberMagnet': 12,
-            'StatorThickness': 13,
-            'CoilLegWidth': 22.5,
-            'CoilInnerWidth1': 30,
-            'CoilInnerWidth2': 30,
-            'MechanicalClearance': 3
-        },
-        'OpenFurl': {
-            'VerticalPlaneAngle': 20,
-            'BracketLength': 300,
-            'BracketWidth': 30,
-            'BracketThickness': 5,
-            'BoomLength': 1000,
-            'BoomPipeRadius': 24.15,
-            'BoomPipeThickness': 5,
-            'VaneThickness': 6,
-            'VaneLength': 1200,
-            'VaneWidth': 500,
-            'Offset': 125
-        },
-        'User': {
-            'HubHolesPlacement': 50,
-            'RotorInnerCircle': 32.5,
-            'Holes': 6,
-            'MetalLengthL': 50,
-            'MetalThicknessL': 6,
-            'FlatMetalThickness': 10,
-            'YawPipeRadius': 30.15,
-            'PipeThickness': 5,
-            'ResineRotorMargin': 5,
-            'HubHoles': 6,
-            'HorizontalPlaneAngle': 55
-        }
-    },
-    WindTurbine.H_SHAPE: {
-        'MagnAFPM': {
-            'RotorDiskRadius': 225,
-            'DiskThickness': 10,
-            'MagnetLength': 46,
-            'MagnetWidth': 30,
-            'MagnetThickness': 10,
-            'NumberMagnet': 16,
-            'StatorThickness': 13,
-            'CoilLegWidth': 32,
-            'CoilInnerWidth1': 30,
-            'CoilInnerWidth2': 30,
-            'MechanicalClearance': 3
-        },
-        'OpenFurl': {
-            'VerticalPlaneAngle': 15,
-            'BracketLength': 600,
-            'BracketWidth': 50,
-            'BracketThickness': 6,
-            'BoomLength': 1800,
-            'BoomPipeRadius': 24.15,
-            'BoomPipeThickness': 5,
-            'VaneThickness': 9,
-            'VaneLength': 1200,
-            'VaneWidth': 900,
-            'Offset': 250
-        },
-        'User': {
-            'HubHolesPlacement': 65,
-            'RotorInnerCircle': 47.5,
-            'Holes': 7,
-            'MetalLengthL': 50,
-            'MetalThicknessL': 6,
-            'FlatMetalThickness': 10,
-            'YawPipeRadius': 44.5,
-            'PipeThickness': 5,
-            'ResineRotorMargin': 5,
-            'HubHoles': 7,
-            'HorizontalPlaneAngle': 55
-        }
-    },
-    WindTurbine.STAR_SHAPE: {
-        'MagnAFPM': {
-            'RotorDiskRadius': 350,
-            'DiskThickness': 10,
-            'MagnetLength': 58,
-            'MagnetWidth': 27,
-            'MagnetThickness': 10,
-            'NumberMagnet': 32,
-            'StatorThickness': 15,
-            'CoilLegWidth': 22.4,
-            'CoilInnerWidth1': 40,
-            'CoilInnerWidth2': 27,
-            'MechanicalClearance': 3
-        },
-        'OpenFurl': {
-            'VerticalPlaneAngle': 15,
-            'BracketLength': 600,
-            'BracketWidth': 50,
-            'BracketThickness': 6,
-            'BoomLength': 1800,
-            'BoomPipeRadius': 24.15,
-            'BoomPipeThickness': 5,
-            'VaneThickness': 9,
-            'VaneLength': 1200,
-            'VaneWidth': 900,
-            'Offset': 500
-        },
-        'User': {
-            'HubHolesPlacement': 102.5,
-            'RotorInnerCircle': 81.5,
-            'Holes': 7,
-            'MetalLengthL': 65,
-            'MetalThicknessL': 8,
-            'FlatMetalThickness': 10,
-            'YawPipeRadius': 57.15,
-            'PipeThickness': 6,
-            'ResineRotorMargin': 5,
-            'HubHoles': 8,
-            'HorizontalPlaneAngle': 55
-        }
-    }
-}
+dir_path = Path(__file__).parent.resolve()
+parameters_path = dir_path.joinpath('parameters.json')
+
+
+with open(parameters_path) as f:
+    parameters_by_variant = json.load(f)
 
 
 class CreateSpreadsheetTaskPanel:
@@ -181,7 +64,7 @@ class CreateSpreadsheetTaskPanel:
         layout.addLayout(row2)
 
     def create_rotor_disk_radius_value(self):
-        default_variant = WindTurbine.T_SHAPE
+        default_variant = WindTurbine.T_SHAPE.value
         default_rotor_disk_radius = get_rotor_disk_radius(default_variant)
         return QtGui.QLabel(default_rotor_disk_radius, self.form)
 
@@ -192,8 +75,7 @@ class CreateSpreadsheetTaskPanel:
         combo_box.activated[str].connect(self.handle_combo_box_activated)
         return combo_box
 
-    def handle_combo_box_activated(self, selected_text):
-        selected_variant = WindTurbine(selected_text)
+    def handle_combo_box_activated(self, selected_variant):
         selected_rotor_disk_radius = get_rotor_disk_radius(selected_variant)
         self.rotor_disk_radius_value.setText(selected_rotor_disk_radius)
 
@@ -201,15 +83,14 @@ class CreateSpreadsheetTaskPanel:
         """
         Executed upon clicking "OK" button in FreeCAD Tasks panel.
         """
-        current_text = self.combo_box.currentText()
-        variant = WindTurbine(current_text)
+        variant = self.combo_box.currentText()
         parameters = parameters_by_variant[variant]
         if self.on_close:
-            self.on_close(parameters['MagnAFPM'],
-                          parameters['OpenFurl'],
-                          parameters['User'])
+            self.on_close(parameters['magnafpm'],
+                          parameters['furling'],
+                          parameters['user'])
         Gui.Control.closeDialog()
 
 
-def get_rotor_disk_radius(variant):
-    return str(parameters_by_variant[variant]['MagnAFPM']['RotorDiskRadius'])
+def get_rotor_disk_radius(variant: str):
+    return str(parameters_by_variant[variant]['magnafpm']['RotorDiskRadius'])
