223b1e0be87bd14f7bb80e90d3a55dbf820b791e
G Roques <groques360@gmail.com>
2021-07-21 18:58:32 -0500

    Add TypedDicts for parameter groups

--- openafpm_cad_core/app.py
+++ openafpm_cad_core/app.py
@@ -1,7 +1,52 @@
+from typing import TypedDict
+
 from .create_spreadsheet_document import create_spreadsheet_document
 from .get_default_parameters import get_default_parameters
 from .load_turbine import load_turbine
 from .wind_turbine import WindTurbine
+
+
+class MagnafpmParameters(TypedDict):
+    RotorDiskRadius: float
+    DiskThickness: float
+    MagnetLength: float
+    MagnetWidth: float
+    MagnetThickness: float
+    NumberMagnet: int
+    StatorThickness: float
+    CoilLegWidth: float
+    CoilInnerWidth1: float
+    CoilInnerWidth2: float
+    MechanicalClearance: float
+
+
+class UserParameters(TypedDict):
+    HubHolesPlacement: float
+    RotorInnerCircle: float
+    Holes: float
+    MetalLengthL: float
+    MetalThicknessL: float
+    FlatMetalThickness: float
+    YawPipeRadius: float
+    PipeThickness: float
+    ResineRotorMargin: float
+    HubHoles: float
+    HorizontalPlaneAngle: float
+
+
+class FurlingParameters(TypedDict):
+    VerticalPlaneAngle: float
+    BracketLength: float
+    BracketWidth: float
+    BracketThickness: float
+    BoomLength: float
+    BoomPipeRadius: float
+    BoomPipeThickness: float
+    VaneThickness: float
+    VaneLength: float
+    VaneWidth: float
+    Offset: float
+
 
 __all__ = [
     'create_spreadsheet_document',
@@ -10,9 +55,9 @@ __all__ = [
 ]
 
 
-def visualize(magnafpm_parameters: dict,
-              user_parameters: dict,
-              furling_parameters: dict) -> WindTurbine:
+def visualize(magnafpm_parameters: MagnafpmParameters,
+              user_parameters: UserParameters,
+              furling_parameters: FurlingParameters) -> WindTurbine:
     return load_turbine(
         magnafpm_parameters,
         user_parameters,
