3176ce0e68ae08f395098b0a700426a479a42c87
G Roques <groques360@gmail.com>
2021-07-24 13:58:08 -0500

    Rename parameters.json to default_paramters.json

--- openafpm_cad_core/get_default_parameters.py
+++ openafpm_cad_core/get_default_parameters.py
@@ -19,7 +19,7 @@ def get_default_parameters(variant: WindTurbine) -> Parameters:
     Variant must be one of "T Shape", "H Shape", or "Star Shape".
     """
     dir_path = Path(__file__).parent.resolve()
-    parameters_path = dir_path.joinpath('parameters.json')
+    parameters_path = dir_path.joinpath('default_parameters.json')
 
     with open(parameters_path) as f:
         parameters_by_variant = json.load(f)
