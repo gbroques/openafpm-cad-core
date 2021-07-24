275e9eb0568e5a71b48ac7d8237c0279a7546038
G Roques <groques360@gmail.com>
2021-07-23 19:06:05 -0500

    Add bash script to link macros

---
+++ link_macros.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -------------------------------------------------------------
+# Creates symlinks to ~/.FreeCAD/Mod for each macro in macros/.
+# -------------------------------------------------------------
+
+macros=`find macros -type f`
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for macro in $macros
+do
+    filename=`basename $macro`
+    echo "Creating symlink to $filename:"
+    echo "    ln --symbolic --force "$(pwd)/$macro" ~/.FreeCAD/Macro/$filename"
+    ln --symbolic --force "$(pwd)/$macro" ~/.FreeCAD/Macro/$filename
+    printf "\n"
+done
+IFS=$SAVEIFS
