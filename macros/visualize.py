d5452c0f50a68da50e3aca8059901e1fe416c85e
G Roques <groques360@gmail.com>
2021-09-17 20:50:43 -0500

    Make visualize macro easier to change turbine type

--- macros/visualize.py
+++ macros/visualize.py
@@ -4,15 +4,17 @@ FreeCAD macro to visualize wind turbine using default values.
 from openafpm_cad_core.app import (WindTurbine, get_default_parameters,
                                    visualize)
 
-parameters = get_default_parameters(WindTurbine.T_SHAPE)
+turbine = WindTurbine.STAR_SHAPE
+parameters = get_default_parameters(turbine)
 
 wind_turbine_model = visualize(
     parameters['magnafpm'],
-    parameters['user'], 
+    parameters['user'],
     parameters['furling'])
 
 obj_file_contents = wind_turbine_model.to_obj()
 
-with open('wind-turbine.obj', 'w') as f:
+filename = turbine.value.lower().replace(' ', '-')
+with open(f'{filename}.obj', 'w') as f:
     f.write(obj_file_contents)
-    print('wind-turbine.obj created.')
+    print(f'{filename}.obj created.')
