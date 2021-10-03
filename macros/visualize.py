d01171cd6afa92a6f78b396ab327cf06e7695c3b
G Roques <groques360@gmail.com>
2021-10-03 15:34:15 -0500

    Pass in wind turbine type as argument to visualize macro

--- macros/visualize.py
+++ macros/visualize.py
@@ -1,20 +1,44 @@
 """
 FreeCAD macro to visualize wind turbine using default values.
 """
+from argparse import ArgumentParser
+
 from openafpm_cad_core.app import (WindTurbine, get_default_parameters,
                                    visualize)
 
-turbine = WindTurbine.STAR_SHAPE
-parameters = get_default_parameters(turbine)
 
-wind_turbine_model = visualize(
-    parameters['magnafpm'],
-    parameters['user'],
-    parameters['furling'])
+def create_obj_file(turbine: WindTurbine):
+    parameters = get_default_parameters(turbine)
 
-obj_file_contents = wind_turbine_model.to_obj()
+    wind_turbine_model = visualize(
+        parameters['magnafpm'],
+        parameters['user'],
+        parameters['furling'])
 
-filename = turbine.value.lower().replace(' ', '-')
-with open(f'{filename}.obj', 'w') as f:
-    f.write(obj_file_contents)
-    print(f'{filename}.obj created.')
+    obj_file_contents = wind_turbine_model.to_obj()
+
+    filename = turbine.value.lower().replace(' ', '-')
+    with open(f'{filename}.obj', 'w') as f:
+        f.write(obj_file_contents)
+        print(f'{filename}.obj created.')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description='Export wind turbine to .obj using default values.')
+    parser.add_argument('variant',
+                        metavar='<variant>',
+                        type=str,
+                        choices={'t', 'h', 'star'},
+                        help='Type of wind turbine, T, H, or Star Shape.')
+
+    args = parser.parse_args()
+
+    variant_by_choice = {
+        't': WindTurbine.T_SHAPE,
+        'h': WindTurbine.H_SHAPE,
+        'star': WindTurbine.STAR_SHAPE
+    }
+
+    turbine = variant_by_choice[args.variant]
+    create_obj_file(turbine)
