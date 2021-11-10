2f89dacad05c2e615b00c792d9131d1c3347c217
G Roques <groques360@gmail.com>
2021-11-09 19:14:51 -0600

    Fix find_object_by_label

--- openafpm_cad_core/find_object_by_label.py
+++ openafpm_cad_core/find_object_by_label.py
@@ -1,23 +1,18 @@
-from typing import Callable, Optional
+from typing import Optional
 
 from FreeCAD import Console, Document
 
 __all__ = ['find_object_by_label']
 
 
-@_with_logging
 def find_object_by_label(document: Document, label: str) -> Optional[object]:
+    """Find an object in a document by the given label.
+
+    Returns ``None`` if no object is found.
+    """
     objects = document.getObjectsByLabel(label)
     if len(objects) == 0:
+        Console.PrintError(
+            f'No object with Label "{label}" found in document. Check {document.Name}.FCStd.')
         return None
     return objects[0]
-
-
-def _with_logging(find_object_by_label: Callable[[str], object]) -> Callable[[str], object]:
-    def wrapped(document, label):
-        result = find_object_by_label(document, label)
-        if result is None:
-            Console.PrintError(
-                'No object with Label "{}" found in document. Check {}.FCStd.'.format(label, document.Name))
-        return result
-    return wrapped
