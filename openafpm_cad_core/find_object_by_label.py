a9725e9442d383d7d80b4a72d29bb2157902ff0b
G Roques <groques360@gmail.com>
2021-11-09 19:09:17 -0600

    Refactor file to make export at top

--- openafpm_cad_core/find_object_by_label.py
+++ openafpm_cad_core/find_object_by_label.py
@@ -1,10 +1,19 @@
-import sys
 from typing import Callable, Optional
 
 from FreeCAD import Console, Document
 
+__all__ = ['find_object_by_label']
 
-def _with_error_exit(find_object_by_label: Callable[[str], object]) -> Callable[[str], object]:
+
+@_with_logging
+def find_object_by_label(document: Document, label: str) -> Optional[object]:
+    objects = document.getObjectsByLabel(label)
+    if len(objects) == 0:
+        return None
+    return objects[0]
+
+
+def _with_logging(find_object_by_label: Callable[[str], object]) -> Callable[[str], object]:
     def wrapped(document, label):
         result = find_object_by_label(document, label)
         if result is None:
@@ -12,11 +21,3 @@ def _with_error_exit(find_object_by_label: Callable[[str], object]) -> Callable[
                 'No object with Label "{}" found in document. Check {}.FCStd.'.format(label, label))
         return result
     return wrapped
-
-
-@_with_error_exit
-def find_object_by_label(document: Document, label: str) -> Optional[object]:
-    objects = document.getObjectsByLabel(label)
-    if len(objects) == 0:
-        return None
-    return objects[0]
