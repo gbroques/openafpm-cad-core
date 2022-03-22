5c07531f6ad13f13802c499162eae413805217f0
G Roques <groques360@gmail.com>
2022-03-21 21:20:30 -0500

    Add coil winder parts

--- openafpm_cad_core/load.py
+++ openafpm_cad_core/load.py
@@ -22,6 +22,7 @@ class Assembly(Enum):
     WindTurbine = 'Wind Turbine'
     StatorMold = 'Stator Mold'
     RotorMold = 'Rotor Mold'
+    CoilWinder = 'Coil Winder'
 
 
 def load_assembly(assembly: Assembly,
@@ -31,7 +32,8 @@ def load_assembly(assembly: Assembly,
     load_function_by_assembly = {
         Assembly.WindTurbine: load_turbine,
         Assembly.StatorMold: load_stator_mold,
-        Assembly.RotorMold: load_rotor_mold
+        Assembly.RotorMold: load_rotor_mold,
+        Assembly.CoilWinder: load_coil_winder_mold
     }
     load_function = load_function_by_assembly[assembly]
     return load_function(magnafpm_parameters, furling_parameters, user_parameters)
@@ -83,6 +85,22 @@ def load_stator_mold(magnafpm_parameters: MagnafpmParameters,
 def get_stator_mold_assembly_document_path(documents_path: Path) -> Path:
     return documents_path.joinpath(
         'Alternator', 'Stator', 'Mold', 'Stator_Mold_Assembly.FCStd')
+
+
+def load_coil_winder_mold(magnafpm_parameters: MagnafpmParameters,
+                          furling_parameters: FurlingParameters,
+                          user_parameters: UserParameters) -> Tuple[Document, Document]:
+    return load_root_document(
+        get_coil_winder_assembly_document_path,
+        magnafpm_parameters,
+        furling_parameters,
+        user_parameters
+    )
+
+
+def get_coil_winder_assembly_document_path(documents_path: Path) -> Path:
+    return documents_path.joinpath(
+        'Alternator', 'Stator', 'CoilWinder', 'Stator_CoilWinder_Assembly.FCStd')
 
 
 def load_rotor_mold(magnafpm_parameters: MagnafpmParameters,
