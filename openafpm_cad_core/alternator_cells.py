cee2233d08d609c4fcfb3ea11d4f65378f168c96
G Roques <groques360@gmail.com>
2022-10-22 15:10:31 -0500

    Create function for generating width across corners cells

--- openafpm_cad_core/alternator_cells.py
+++ openafpm_cad_core/alternator_cells.py
@@ -4,6 +4,61 @@ from .spreadsheet import Alignment, Cell, Style
 
 __all__ = ['alternator_cells']
 
+
+def generate_width_across_corner_cells(
+        parameter_name: str,
+        prefix: str,
+        radius: str,
+) -> List[List[Cell]]:
+    return [
+        [
+            Cell(parameter_name),
+            Cell(f'Select C (MAX) based on {radius}.'),
+            Cell('https://www.atlrod.com/metric-hex-bolt-dimensions/')
+        ],
+        [
+            Cell(f'{prefix}range7'),
+            Cell(f'={radius} <= 18 ? 63.51 : 75.05',
+                 alias=f'{prefix}range7'),
+        ],
+        [
+            Cell(f'{prefix}range6'),
+            Cell(f'={radius} <= 15 ? 53.12 : {prefix}range7',
+                 alias=f'{prefix}range6')
+        ],
+        [
+            Cell(f'{prefix}range5'),
+            Cell(f'={radius} <= 12 ? 41.57 : {prefix}range6',
+                 alias=f'{prefix}range5'),
+        ],
+        [
+            Cell(f'{prefix}range4'),
+            Cell(f'={radius} <= 10 ? 34.64 : {prefix}range5',
+                 alias=f'{prefix}range4'),
+        ],
+        [
+            Cell(f'{prefix}range3'),
+            Cell(f'={radius} <= 8 ? 27.71 : {prefix}range4',
+                 alias=f'{prefix}range3'),
+        ],
+        [
+            Cell(f'{prefix}range2'),
+            Cell(f'={radius} <= 7 ? 24.25 : {prefix}range3',
+                 alias=f'{prefix}range2'),
+        ],
+        [
+            Cell(f'{prefix}range1'),
+            Cell(f'={radius} <= 6 ? 20.78 : {prefix}range2',
+                 alias=f'{prefix}range1'),
+        ],
+        [
+            Cell(parameter_name),
+            Cell(f'={radius} <= 5 ? 18.48 : {prefix}range1',
+                 alias=parameter_name),
+        ]
+    ]
+
+
 alternator_cells: List[List[Cell]] = [
     [
         Cell('Inputs', styles=[Style.UNDERLINE, Style.BOLD])
@@ -428,31 +483,11 @@ alternator_cells: List[List[Cell]] = [
         Cell('6',
              alias='RotorMoldLidThickness')
     ],
-    [
-        # Select C (MAX) based on HubHoles.
-        # https://www.atlrod.com/metric-hex-bolt-dimensions/
-        Cell('RotorMoldBoltWidthAcrossCorners')
-    ],
-    [
-        Cell('75.05',
-             alias='RMBWAFmax'),
-        Cell('=HubHoles <= 18 ? 63.51 : RMBWAFmax',
-             alias='RMBWAFrange7'),
-        Cell('=HubHoles <= 15 ? 53.12 : RMBWAFrange7',
-             alias='RMBWAFrange6'),
-        Cell('=HubHoles <= 12 ? 41.57 : RMBWAFrange6',
-             alias='RMBWAFrange5'),
-        Cell('=HubHoles <= 10 ? 34.64 : RMBWAFrange5',
-             alias='RMBWAFrange4'),
-        Cell('=HubHoles <= 8 ? 27.71 : RMBWAFrange4',
-             alias='RMBWAFrange3'),
-        Cell('=HubHoles <= 7 ? 24.25 : RMBWAFrange3',
-             alias='RMBWAFrange2'),
-        Cell('=HubHoles <= 6 ? 20.78 : RMBWAFrange2',
-             alias='RMBWAFrange1'),
-        Cell('=HubHoles <= 5 ? 18.48 : RMBWAFrange1',
-             alias='RotorMoldBoltWidthAcrossCorners'),
-    ],
+    *generate_width_across_corner_cells(
+        'RotorMoldBoltWidthAcrossCorners',
+        'RMBWAC',
+        'HubHoles'
+    ),
     [
         Cell('DistanceBetweenRotorMoldScrewsAndResin'),
         Cell('RotorMoldScrewHolesCircumradius'),
