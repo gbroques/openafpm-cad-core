c3d40acf99b6c4ca2bc2d9988e73cd7165644fcc
G Roques <groques360@gmail.com>
2023-08-26 18:55:06 -0500

    Improve link_macros bash script with OS awareness

--- link_macros.bash
+++ link_macros.bash
@@ -1,7 +1,39 @@
 #!/bin/bash
-# -----------------------------------------------------------------------
-# Creates a symlink to FreeCAD Macro directory for each macro in macros/.
-# -----------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# Convenience script to create symlinks to FreeCAD's default macro directory
+# for each macro in macros/.
+#                                               
+# Supports Linux operating systems.
+#
+# Contributions to support MacOS & Windows are welcomed.
+# --------------------------------------------------------------------------
+
+# Exit on error.
+set -e
+
+is_linux()
+{
+    [ "$(uname --kernel-name)" = "Linux" ]
+}
+
+is_macOS()
+{
+     "$(uname --kernel-name)" = "Darwin" ]
+}
+
+if is_linux; then
+    macro_dir=`realpath ~/.local/share/FreeCAD/Macro`
+elif is_macOS; then
+    echo "Error: MacOS not supported. Please contribute changes on GitHub." >&2
+    echo "See default Macro directory for MacOS here:" >&2
+    echo "https://wiki.freecad.org/How_to_install_macros#Default_directory" >&2
+    exit 1
+else # Windows
+    echo "Error: Windows not supported. Please contribute changes on GitHub." >&2
+    echo "See default Macro directory for Windows here:" >&2
+    echo "https://wiki.freecad.org/How_to_install_macros#Default_directory" >&2
+    exit 1
+fi
 
 macros=`find macros -type f`
 
@@ -10,10 +42,7 @@ IFS=$(echo -en "\n\b")
 for macro in $macros
 do
     filename=`basename $macro`
-    echo "Creating symlink to $filename:"
-    macro_dir="~/.local/share/FreeCAD/Macro"
-    echo "    ln --symbolic --force $(pwd)/$macro $macro_dir/$filename"
-    ln --symbolic --force "$(pwd)/$macro $macro_dir/$filename"
-    printf "\n"
+    echo "ln --symbolic --force $(pwd)/$macro $macro_dir/$filename"
+    ln --symbolic --force $(pwd)/$macro $macro_dir/$filename
 done
 IFS=$SAVEIFS
