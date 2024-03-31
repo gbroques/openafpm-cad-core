e610985525102341b136b8573acabd2b559ccbae
G Roques <groques360@gmail.com>
2024-03-30 20:30:20 -0500

    Use more standard birch plywood thicknesses

--- openafpm_cad_core/blade_cells.py
+++ openafpm_cad_core/blade_cells.py
@@ -66,73 +66,43 @@ blade_cells: List[List[Cell]] = [
         Cell('Round to nearest plywood thickness')
     ],
     [
-        Cell('Range13'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 37.5 ? {PlywoodThickness.MM_35.value} : {PlywoodThickness.MM_40.value}',
-             alias='Range13')
-    ],
-    [
-        Cell('Range12'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 32.5 ? {PlywoodThickness.MM_30.value} : Range13',
-             alias='Range12')
-    ],
-    [
-        Cell('Range11'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 28.5 ? {PlywoodThickness.MM_27.value} : Range12',
-             alias='Range11')
-    ],
-    [
-        Cell('Range10'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 26 ? {PlywoodThickness.MM_24.value} : Range11',
-             alias='Range10')
-    ],
-    [
-        Cell('Range9'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 22.5 ? {PlywoodThickness.MM_21.value} : Range10',
-             alias='Range9')
-    ],
-    [
-        Cell('Range8'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 20.5 ? {PlywoodThickness.MM_20.value} : Range9',
-             alias='Range8')
-    ],
-    [
         Cell('Range7'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 19 ? {PlywoodThickness.MM_18.value} : Range8',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 28.5 ? {PlywoodThickness.MM_27.value} : {PlywoodThickness.MM_30}',
              alias='Range7')
     ],
     [
         Cell('Range6'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 16.5 ? {PlywoodThickness.MM_15.value} : Range7',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 25.5 ? {PlywoodThickness.MM_24.value} : Range7',
              alias='Range6')
     ],
     [
         Cell('Range5'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 13.5 ? {PlywoodThickness.MM_12.value} : Range6',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 22.5 ? {PlywoodThickness.MM_21.value} : Range6',
              alias='Range5')
     ],
     [
         Cell('Range4'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 11 ? {PlywoodThickness.MM_10.value} : Range5',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 19.5 ? {PlywoodThickness.MM_18.value} : Range5',
              alias='Range4')
     ],
     [
         Cell('Range3'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 9.5 ? {PlywoodThickness.MM_9.value} : Range4',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 16.5 ? {PlywoodThickness.MM_15.value} : Range4',
              alias='Range3')
     ],
     [
         Cell('Range2'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 8.5 ? {PlywoodThickness.MM_8.value} : Range3',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 13.5 ? {PlywoodThickness.MM_12.value} : Range3',
              alias='Range2')
     ],
     [
         Cell('Range1'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 7 ? {PlywoodThickness.MM_6.value} : Range2',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 10.5 ? {PlywoodThickness.MM_9.value} : Range2',
              alias='Range1')
     ],
     [
         Cell('BladeAssemblyPlateThickness'),
-        Cell(f'=UnroundedBladeAssemblyPlateThickness < 5 ? {PlywoodThickness.MM_4.value} : Range1',
+        Cell(f'=UnroundedBladeAssemblyPlateThickness < 7.5 ? {PlywoodThickness.MM_6.value} : Range1',
              alias='BladeAssemblyPlateThickness')
     ],
     [
--- openafpm_cad_core/plywood_thickness.py
+++ openafpm_cad_core/plywood_thickness.py
@@ -3,21 +3,15 @@ from enum import Enum, unique
 
 @unique
 class PlywoodThickness(int, Enum):
-    """Enumeration of plywood thicknesses for a couple of standard materials in millimeters.
+    """Enumeration of birch plywood thicknesses in millimeters.
     """
-    MM_4 = 4
     MM_6 = 6
-    MM_8 = 8
     MM_9 = 9
-    MM_10 = 10
     MM_12 = 12
     MM_15 = 15
     MM_18 = 18
-    MM_20 = 20
     MM_21 = 21
     MM_24 = 24
-    MM_25 = 25
     MM_27 = 27
     MM_30 = 30
-    MM_35 = 35
-    MM_40 = 40
+
