5595f845a210001b1e4f2691b82a999502e9cde9
G Roques <groques360@gmail.com>
2024-04-14 14:04:25 -0500

    Add number of rotor mould bolts to dimension tables

--- openafpm_cad_core/get_dimension_tables.py
+++ openafpm_cad_core/get_dimension_tables.py
@@ -413,6 +413,7 @@ def create_rotor_mold_dimensions_table(spreadsheet_document: Document) -> Elemen
              round_and_format_length(spreadsheet_document.Alternator.RotorMoldSurroundThickness)),
             ('Island thickness', round_and_format_length(
                 spreadsheet_document.Alternator.RotorMoldIslandThickness)),
+            ('Number of bolts', spreadsheet_document.Alternator.NumberOfRotorMoldBolts),
         ],
         book_reference_template % 'page 42 left-hand side'
     )
