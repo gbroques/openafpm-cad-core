390bc3b8a30779726722c79d4bb8013ae20133e8
G Roques <groques360@gmail.com>
2024-05-28 21:06:44 -0500

    Define constants for H & Star Shape lower bounds
    Add __all__ to module

--- openafpm_cad_core/map_rotor_disk_radius_to_wind_turbine_shape.py
+++ openafpm_cad_core/map_rotor_disk_radius_to_wind_turbine_shape.py
@@ -1,10 +1,15 @@
 from .wind_turbine_shape import WindTurbineShape
 
+__all__ = ['map_rotor_disk_radius_to_wind_turbine_shape']
+
+H_SHAPE_LOWER_BOUND = 187.5
+STAR_SHAPE_LOWER_BOUND = 275
+
 
 def map_rotor_disk_radius_to_wind_turbine_shape(rotor_disk_radius: float) -> WindTurbineShape:
-    if rotor_disk_radius < 187.5:
+    if rotor_disk_radius < H_SHAPE_LOWER_BOUND:
         return WindTurbineShape.T
-    elif rotor_disk_radius < 275:
+    elif rotor_disk_radius < STAR_SHAPE_LOWER_BOUND:
         return WindTurbineShape.H
     else:
         return WindTurbineShape.STAR
