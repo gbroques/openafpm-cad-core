4c637110d49df70194df02643bba1813f4e39a2d
G Roques <groques360@gmail.com>
2025-10-29 19:55:13 -0500

    Reference constants from WindTurbineShape for H & Star shape lower bounds

--- openafpm_cad_core/upsert_spreadsheet_document.py
+++ openafpm_cad_core/upsert_spreadsheet_document.py
@@ -10,57 +10,63 @@ from .fastener_cells import get_fastener_cells
 from .high_end_stop_cells import high_end_stop_cells
 from .hub_cells import hub_cells
 from .low_end_stop_cells import low_end_stop_cells
-from .parameter_groups import (FurlingParameters, MagnafpmParameters,
-                               UserParameters)
+from .parameter_groups import FurlingParameters, MagnafpmParameters, UserParameters
 from .parameters_by_key_to_cells import parameters_by_key_to_cells
 from .spreadsheet import Cell, populate_spreadsheet
 from .tail_cells import tail_cells
 from .wind_turbine_cells import wind_turbine_cells
 from .yaw_bearing_cells import yaw_bearing_cells
+from .wind_turbine_shape import H_SHAPE_LOWER_BOUND, STAR_SHAPE_LOWER_BOUND
 
-__all__ = ['upsert_spreadsheet_document']
+__all__ = ["upsert_spreadsheet_document"]
 
 
-def upsert_spreadsheet_document(path: Path,
-                                magnafpm_parameters: MagnafpmParameters,
-                                furling_parameters: FurlingParameters,
-                                user_parameters: UserParameters) -> Document:
+def upsert_spreadsheet_document(
+    path: Path,
+    magnafpm_parameters: MagnafpmParameters,
+    furling_parameters: FurlingParameters,
+    user_parameters: UserParameters,
+) -> Document:
     cells_by_spreadsheet_name = get_cells_by_spreadsheet_name(
-        magnafpm_parameters,
-        furling_parameters,
-        user_parameters)
+        magnafpm_parameters, furling_parameters, user_parameters
+    )
     return upsert_document(path, cells_by_spreadsheet_name)
 
 
-def get_cells_by_spreadsheet_name(magnafpm_parameters: MagnafpmParameters,
-                                  furling_parameters: FurlingParameters,
-                                  user_parameters: UserParameters) -> Dict[str, List[List[Cell]]]:
-    cells = parameters_by_key_to_cells({
-        'MagnAFPM': magnafpm_parameters,
-        'Furling': furling_parameters,
-        'User': user_parameters,
-        'Shape': {
-           'HShapeLowerBound': 187.5,
-           'StarShapeLowerBound': 275,
-           'WindTurbineShape': '=RotorDiskRadius < HShapeLowerBound ? <<T>> : (RotorDiskRadius < StarShapeLowerBound ? <<H>> : <<Star>>)'
-        },
-    })
+def get_cells_by_spreadsheet_name(
+    magnafpm_parameters: MagnafpmParameters,
+    furling_parameters: FurlingParameters,
+    user_parameters: UserParameters,
+) -> Dict[str, List[List[Cell]]]:
+    cells = parameters_by_key_to_cells(
+        {
+            "MagnAFPM": magnafpm_parameters,
+            "Furling": furling_parameters,
+            "User": user_parameters,
+            "Shape": {
+                "HShapeLowerBound": H_SHAPE_LOWER_BOUND,
+                "StarShapeLowerBound": STAR_SHAPE_LOWER_BOUND,
+                "WindTurbineShape": "=RotorDiskRadius < HShapeLowerBound ? <<T>> : (RotorDiskRadius < StarShapeLowerBound ? <<H>> : <<Star>>)",
+            },
+        }
+    )
     return {
-        'Spreadsheet': cells,
-        'Fastener': get_fastener_cells(),
-        'Hub': hub_cells,
-        'Blade': blade_cells,
-        'Alternator': alternator_cells,
-        'YawBearing': yaw_bearing_cells,
-        'Tail': tail_cells,
-        'LowEndStop': low_end_stop_cells,
-        'HighEndStop': high_end_stop_cells,
-        'WindTurbine': wind_turbine_cells
+        "Spreadsheet": cells,
+        "Fastener": get_fastener_cells(),
+        "Hub": hub_cells,
+        "Blade": blade_cells,
+        "Alternator": alternator_cells,
+        "YawBearing": yaw_bearing_cells,
+        "Tail": tail_cells,
+        "LowEndStop": low_end_stop_cells,
+        "HighEndStop": high_end_stop_cells,
+        "WindTurbine": wind_turbine_cells,
     }
 
 
-def upsert_document(path: Path,
-                    cells_by_spreadsheet_name: Dict[str, List[List[Cell]]]) -> Document:
+def upsert_document(
+    path: Path, cells_by_spreadsheet_name: Dict[str, List[List[Cell]]]
+) -> Document:
     if path.exists():
         document = App.openDocument(str(path))
     else:
@@ -72,12 +78,13 @@ def upsert_document(path: Path,
     return document
 
 
-def populate_spreadsheets(document: Document,
-                          cells_by_spreadsheet_name: Dict[str, List[List[Cell]]]) -> None:
+def populate_spreadsheets(
+    document: Document, cells_by_spreadsheet_name: Dict[str, List[List[Cell]]]
+) -> None:
     for spreadsheet_name, cells in cells_by_spreadsheet_name.items():
         sheet = document.getObject(spreadsheet_name)
         if sheet is None:
-            sheet = document.addObject('Spreadsheet::Sheet', spreadsheet_name)
+            sheet = document.addObject("Spreadsheet::Sheet", spreadsheet_name)
         else:
             sheet.clearAll()
         populate_spreadsheet(sheet, cells)
